a1d3b69d546b1946e5b66276f07c80d57103058b
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-09-14 05:08:20 +0600

    UPDATE - skeleton for dark mood

--- src/skeleton/TodoCartSkeleton.tsx
+++ src/skeleton/TodoCartSkeleton.tsx
@@ -1,18 +1,18 @@
 const TodoCartSkeleton = () => {
     return (
-        <div className="animate-pulse bg-white rounded-xl shadow-sm p-4 space-y-4 border border-gray-200">
-            <div className="h-5 w-3/4 bg-gray-200 rounded"></div>
-            <div className="h-4 w-5/6 bg-gray-200 rounded"></div>
-            <div className="h-4 w-2/3 bg-gray-200 rounded"></div>
+        <div className="animate-pulse bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 space-y-4 border border-gray-200 dark:border-gray-700">
+            <div className="h-5 w-3/4 bg-gray-200 dark:bg-gray-700 rounded"></div>
+            <div className="h-4 w-5/6 bg-gray-200 dark:bg-gray-700 rounded"></div>
+            <div className="h-4 w-2/3 bg-gray-200 dark:bg-gray-700 rounded"></div>
             <div className="flex justify-between items-center pt-3">
-                <div className="h-6 w-16 bg-gray-200 rounded"></div>
-                <div className="h-6 w-16 bg-gray-200 rounded"></div>
+                <div className="h-6 w-16 bg-gray-200 dark:bg-gray-700 rounded"></div>
+                <div className="h-6 w-16 bg-gray-200 dark:bg-gray-700 rounded"></div>
             </div>
             <div className="flex space-x-2 pt-2">
-                <div className="h-4 w-10 bg-gray-200 rounded"></div>
-                <div className="h-4 w-12 bg-gray-200 rounded"></div>
+                <div className="h-4 w-10 bg-gray-200 dark:bg-gray-700 rounded"></div>
+                <div className="h-4 w-12 bg-gray-200 dark:bg-gray-700 rounded"></div>
             </div>
-            <div className="h-3 w-1/2 bg-gray-200 rounded pt-2"></div>
+            <div className="h-3 w-1/2 bg-gray-200 dark:bg-gray-700 rounded pt-2"></div>
         </div>
     );
 };
